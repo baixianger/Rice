7ed34a8ad0004a19b98a3f38aabf0bffbeaff0eb
baixianger <58243749+baixianger@users.noreply.github.com>
2025-07-07 17:25:03 +0200

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,9 @@
-import prisma from "@/lib/db";
 import { Button } from "@/components/ui/button";
-import { Prisma } from "@/generated/prisma";
 
-type UserWithPosts = Prisma.UserGetPayload<{
-  include: {
-    posts: true;
-  };
-}>;
-
-export default async function Home() {
-  const users: UserWithPosts[] = await prisma.user.findMany({
-    include: {
-      posts: true,
-    },
-  });
-
+export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
       <Button>Button</Button>
-      <p>{users.length}</p>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <p>{user.name}</p>
-            <p>{user.email}</p>
-            <ul>
-              {user.posts.map((post) => (
-                <li key={post.id}>
-                  <p>{post.title}</p>
-                  <p>{post.content}</p>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
