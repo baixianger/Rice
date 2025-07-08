364bac1f5db48769652ab22b3dd365fdb89ead91
baixianger <58243749+baixianger@users.noreply.github.com>
2025-07-08 17:08:49 +0200

    Leveraging Suspense by using the useSuspenseQuery hook

--- src/app/prefetch-query-demo/client-greeting.tsx
+++ src/app/prefetch-query-demo/client-greeting.tsx
@@ -2,11 +2,11 @@
 
 // https://trpc.io/docs/client/tanstack-react-query/server-components#using-your-api
 // <-- hooks can only be used in client components
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { useTRPC } from '@/trpc/client';
 export function ClientGreeting() {
   const trpc = useTRPC();
-  const greeting = useQuery(trpc.hello.queryOptions({ text: '白翔' }));
+  const greeting = useSuspenseQuery(trpc.hello.queryOptions({ text: '白翔' }));
   if (!greeting.data) return <div>Loading...</div>;
   return <div>{greeting.data.greeting}</div>;
 }
--- src/app/prefetch-query-demo/page.tsx
+++ src/app/prefetch-query-demo/page.tsx
@@ -1,5 +1,7 @@
 // https://trpc.io/docs/client/tanstack-react-query/server-components#using-your-api
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import { getQueryClient, trpc } from '@/trpc/server';
 import { ClientGreeting } from './client-greeting';
 export default async function Home() {
@@ -12,9 +14,11 @@ export default async function Home() {
   );
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <div>...</div>
-      {/** ... */}
-      <ClientGreeting />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ErrorBoundary fallback={<div>Something went wrong</div>}>
+          <ClientGreeting />
+        </ErrorBoundary>
+      </Suspense>
     </HydrationBoundary>
   );
 }
